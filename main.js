66654e85cfd3925d4bcd41063b9591a5f346643b
Tonio <antoine.meeus@hotmail.com>
2018-12-13 10:48:45 +0100

    Final version with vueJS integration, better UX and search filter. Carefull that the modals open the link via proxy for a cors bypass

--- main.js
+++ main.js
@@ -74,19 +74,33 @@ const missed_votes_fName = "missed_votes";
 const republican_str_id = "R";
 const democrat_str_id = "D";
 const independent_str_id = "I";
-
 // Statistics - Size in percent for evaluation group
 const PCT_RATIO = 10;
 
 //API personal access key - 5000 access/day permitted
 const PROPUBLICA_API_KEY = "1utK9P6TpuO6BDzWDp8qE5UcKAiDiQ6xdIkavhNy";
 const WEBSITE_API = "https://api.propublica.org/congress/v1/";
+const X_OPTION_BYPASS = "http://x-frame-options-bypass.herokuapp.com/?url=";
 const CONGRESS = 113;
 
-var init = {
-    headers: new Headers({
-        'x-api-key': PROPUBLICA_API_KEY
-    })
+// Modal Plugin
+const VuedalsBus = Vuedals.Bus;
+const Component = Vuedals.Component;
+const Plugin = Vuedals.default;
+
+Vue.use(Plugin);
+
+const websiteModal = {
+    name: 'webstie_modal',
+
+    props: ['url', 'ori_url'],
+
+    template: `<div>
+        <p>Open the Website on a new tab <a :href="ori_url" target="_blank">{{ori_url}}</a></p>
+        <iframe :src="url" style="height:70vh; width:98%;" frameBorder="0"></iframe>  
+        
+    </div>`
+
 };
 
 
@@ -96,6 +110,10 @@ var app = new Vue({
         members: [],
         party_filters: [republican_str_id, democrat_str_id, independent_str_id],
         state_filter: "All",
+        search: "",
+        sortKey: 'last_name',
+        reverse: false,
+
         partyInfo: {
             "Democrats": {
                 "number": 0,
@@ -123,11 +141,29 @@ var app = new Vue({
         },
         chamber: "",
         url: "",
-        loading_finished:false,
+        loading_finished: false,
 
 
     },
+    components: {
+        vuedals: Component,
+    },
+
     methods: {
+        openModal(url, FullName) {
+            console.log(url);
+            app.$vuedals.open({
+                title: FullName + ' - Website',
+                size: "xl",
+                component: websiteModal,
+                props: {
+                    url: X_OPTION_BYPASS + url + "",
+                    ori_url: url,
+                    fname: FullName,
+                }
+            });
+        },
+
         getStateFullName: function (stateAbbr) {
             if (typeof FullNameStatesD != "undefined") {
                 return FullNameStatesD[stateAbbr];
@@ -135,8 +171,13 @@ var app = new Vue({
             return stateAbbr;
 
         },
+
         getLiveData: function () {
-            //Fetch Live Data from API
+            var init = {
+                headers: new Headers({
+                    'x-api-key': PROPUBLICA_API_KEY
+                })
+            };
             fetch(this.url, init)
                 .then(function (resp) {
                     if (resp.ok) return resp.json();
@@ -153,13 +194,19 @@ var app = new Vue({
                 });
         },
 
+        sortBy: function (argSortKey) {
+            this.reverse = (this.sortKey == argSortKey) ? !this.reverse : false;
+
+            this.sortKey = argSortKey;
+        },
+
         calculateStatistic: function () {
             var self = this;
             if (this.members.length > 0) {
 
                 //Party Info Number+Average+Total
                 this.members.forEach(function (member) {
-                    if (member.party === democrat_str_id) {                        
+                    if (member.party === democrat_str_id) {
                         self.partyInfo.Democrats.number += 1;
                         self.partyInfo.Democrats.average += member[loyalty_votes_fName];
                     }
@@ -185,48 +232,58 @@ var app = new Vue({
                 } else this.partyInfo.Independents.average = 0;
 
                 this.partyInfo.Total.number = this.partyInfo.Democrats.number + this.partyInfo.Republicans.number + this.partyInfo.Independents.number;
-                var chambersNumber = this.partyInfo.averageVotes_with_party_Independent == 0 ? 2 : 3;
+                var chambersNumber = this.partyInfo.Independents.average == 0 ? 2 : 3;
 
-                this.partyInfo.Total.average = ((this.partyInfo.Democrats.average + this.partyInfo.Republicans.average + this.partyInfo.Independents.average) / chambersNumber);
+                this.partyInfo.Total.average = (this.members
+                    .map(member => {
+                        return member[loyalty_votes_fName]
+                    })
+                    .reduce((acc, currValue) => {
+                        return acc + currValue;
+                    }, 0)) / (this.partyInfo.Total.number);
 
                 //Fixes the average pct to 2 digit
                 for (let party of Object.keys(this.partyInfo)) {
                     this.partyInfo[party].average = (this.partyInfo[party].average).toFixed(2);
                 }
 
-
                 //Get Chamber Info - Least&Most Engaged&Loyal
-
                 var length_list = this.members.length;
                 var target_index = Math.round(length_list * PCT_RATIO / 100);
 
-                function compareNext(arr, j) {
-                    if (arr[j] === arr[j + 1]) return compareNext(arr, j + 1);
-                    else return arr.slice(0, j);
+                function compareNext(arr, field, j) {
+                    if (arr[j][field] === arr[j + 1][field]) {
+
+                        return compareNext(arr, field, j + 1);
+                    } else return arr.slice(0, j + 1);
                 }
 
-                function comparePrev(arr, j) {
-                    if (arr[j] === arr[j - 1]) return compareNext(arr, j - 1);
-                    else return arr.slice(j + 1, arr.length).reverse();
+                function comparePrev(arr, field, j) {
+                    if (arr[j][field] === arr[j - 1][field]) {
+
+                        return comparePrev(arr, field, j - 1);
+                    } else return arr.slice(j).reverse();
+
+
+
                 }
                 var tempList = this.members;
                 var loyalty_sorted_list = [...tempList].sort(function (a, b) {
                     return a[loyalty_votes_fName] - b[loyalty_votes_fName]
                 });
-
                 var engaged_sorted_list = [...tempList].sort(function (a, b) {
                     return a[engaged_votes_fName] - b[engaged_votes_fName]
                 });
 
-                this.chamber_statistics.least_loyal_members = compareNext(loyalty_sorted_list, target_index);
-                this.chamber_statistics.most_loyal_members = comparePrev(loyalty_sorted_list, loyalty_sorted_list.length - 1 - target_index);
+                this.chamber_statistics.least_loyal_members = compareNext(loyalty_sorted_list, loyalty_votes_fName, target_index - 1);
+                this.chamber_statistics.most_loyal_members = comparePrev(loyalty_sorted_list, loyalty_votes_fName, loyalty_sorted_list.length - target_index);
 
-                this.chamber_statistics.most_engaged_members = compareNext(engaged_sorted_list, target_index);
-                this.chamber_statistics.least_engaged_members = comparePrev(engaged_sorted_list, loyalty_sorted_list.length - 1 - target_index);
-                
-                
+                this.chamber_statistics.most_engaged_members = compareNext(engaged_sorted_list, engaged_votes_fName, target_index - 1);
+                this.chamber_statistics.least_engaged_members = comparePrev(engaged_sorted_list, engaged_votes_fName, loyalty_sorted_list.length - target_index);
+
+
                 //Finished all calculation so loader can hide now
-                this.loading_finished=true;
+                this.loading_finished = true;
             }
 
         }
@@ -241,9 +298,35 @@ var app = new Vue({
 
         upToDateTable: function () {
             var self = this;
-            var listToFilter = this.members;
-            return listToFilter.filter(member =>
-                (self.party_filters.includes(member.party) && (self.state_filter === "All" || self.state_filter === member.state)));
+            var searchFilter = this.search.toLowerCase();
+            var listToFilter = [];
+
+            //Filtering with the filter
+            listToFilter = this.members.filter(member => {
+                
+                var searchTextIsIncluded = (member.last_name + " " + member.first_name + " " + (member.middle_name | "") + " " + member.party + " " + member.state + member.seniority + " " + member.votes_with_party_pct).toLowerCase().includes(searchFilter);
+                
+                
+                return (self.party_filters.includes(member.party) && (self.state_filter === "All" || self.state_filter === member.state) && searchTextIsIncluded);
+            });
+
+            //Filtering with the search query
+            //            var listToFilter2 = listToFilter.filter(member => (JSON.stringify(member)).toLowerCase().includes(searchFilter));
+
+
+            //Sort the filtered array from columns           
+            var modifier = this.reverse ? -1 : 1;
+
+            var listFiltered = listToFilter.sort((a, b) => {
+                //Check if is number or string inside the string
+                let c = (isNaN(a[this.sortKey]) ? a[this.sortKey] : +a[this.sortKey]);
+                let d = (isNaN(b[this.sortKey]) ? b[this.sortKey] : +b[this.sortKey]);
+                if (c < d) return -1 * modifier;
+                if (c > d) return 1 * modifier;
+                return 0;
+            });
+
+            return listFiltered;
         },
         get_partyInfo: function () {
             return this.partyInfo;
@@ -266,12 +349,11 @@ var app = new Vue({
     created: function () {
         if (window.location.href.includes("senate")) {
             this.chamber = "senate";
-            //console.log("Hey we are on the senate page");
-        }
+
+        };
         if (window.location.href.includes("house")) {
             this.chamber = "house";
-            //console.log("Hey we are on the house page");
-        }
+        };
         this.url = WEBSITE_API + CONGRESS + "/" + this.chamber + "/members.json";
 
     },
@@ -308,6 +390,25 @@ var app = new Vue({
         }
     }
 
-
-
 });
+
+
+//Sticky header responsive:
+
+var myStickyHeader = document.querySelector("#myStickyHeader");
+var myStickyFilter = document.querySelector("#myStickyFilter");
+
+if (myStickyFilter && myStickyHeader) {
+    window.addEventListener("resize", updatePosition, false);
+
+    updatePosition();
+
+    function updatePosition() {
+        var desiredTopPosition = myStickyFilter.getBoundingClientRect().height;
+        var theaders = myStickyHeader.getElementsByTagName("th");
+        for (let i = 0; i < theaders.length; i++) {
+            theaders[i].style.top = desiredTopPosition + 50 + "px";
+        }
+
+    }
+}
